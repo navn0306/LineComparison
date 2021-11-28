868775f1e46f44c2e5ca6bd5f6c43bc8bc75b98e
Navneet <navn0306@gmail.com>
2021-11-28 22:40:27 +0530

    ADD: Added functionality to compare the lenght of two entered line cordinate

--- src/com/bridge/LineComparison/LineComparision.java
+++ src/com/bridge/LineComparison/LineComparision.java
@@ -29,7 +29,7 @@ public class LineComparision {
         System.out.println("y:- ");
         y2 = sc.nextInt();
         Cordinates cordinatesOfLine2 = new Cordinates(x1, y1, x2, y2);
-        cordinatesOfLine1.checkEquality(cordinatesOfLine2);
+        cordinatesOfLine1.compareLength(cordinatesOfLine2);
     }
 
     private static class Cordinates {
@@ -41,13 +41,16 @@ public class LineComparision {
             this.y2 = y2;
         }
 
-        public void checkEquality(Cordinates cordinates) {
+        public void compareLength(Cordinates cordinates) {
             Double lengthOfLine1 = Math.sqrt(Math.pow((this.x2 - this.x1),2) + Math.pow((this.y2 - this.y1),2)),
                     lengthOfLine2 = Math.sqrt(Math.pow((cordinates.x2 - cordinates.x1),2) + Math.pow((cordinates.y2 - cordinates.y1),2));
-            if(lengthOfLine1.equals(lengthOfLine2)) {
+
+            if(lengthOfLine1.compareTo(lengthOfLine2) == 0) {
                 System.out.println("Both lines have equal length!");
+            } else if(lengthOfLine1.compareTo(lengthOfLine2) > 0) {
+                System.out.println("1st line is greater than 2nd line!");
             } else {
-                System.out.println("Both lines have un2equal length!");
+                System.out.println("2nd line is greater than 1st line!");
             }
         }
     }
