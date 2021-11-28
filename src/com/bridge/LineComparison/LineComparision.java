55820c55a5d73aa5ce5ad628d397536423ee8751
Navneet <navn0306@gmail.com>
2021-11-28 15:55:48 +0530

    ADD: Added welcome message for master

---
+++ src/com/bridge/LineComparison/LineComparision.java
@@ -0,0 +1,8 @@
+package com.bridge.LineComparison;
+
+public class LineComparision {
+    public static void main(String[] args) {
+
+        System.out.println("\n Welcome to Line Comparison Computation Program on Master Branch ");
+    }
+}
